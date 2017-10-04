cac153be768ec30d29c65d70a6e62f08759cac81
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-10-04 13:59:46 -0700

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,1 +1,11 @@
+SetTime <-strptime(paste(Needed$Date, Needed$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+FinalData <- cbind(Needed, SetTime)
+head(FinalData) #look at 5 rows
+str(FinalData)# structure of Dataset
+plot(FinalData$SetTime, FinalData$Global_active_power, xlab = " ", 
+     type = "l",ylab = "Global Active Power (kilowatts)")
 
+#save Plot
+ dev.copy(png, "Plot2.png", height =480, width = 480)
+ dev.off()
+ 
